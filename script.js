3b0fbf1a046b1d8533c7b789e31013e1b71e4692
valerie-wa <wangvhj@gmail.com>
2026-09-12 20:43:29 -0400

    js for checkbox and del button

--- script.js
+++ script.js
@@ -16,6 +16,32 @@ todoForm.addEventListener('submit', function(event) {
 
 function addTask(task) {
     const liItem = document.createElement('li');
-    liItem.textContent = task;
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+
+    liItem.appendChild(checkBox);
+    liItem.appendChild(taskText);
+    liItem.appendChild(deleteButton);
+
     todoList.appendChild(liItem);
+
+    checkBox.addEventListener('change', function(){
+        if(this.checked){
+            taskText.style.textDecoration = 'line-through';
+        } 
+        if(!this.checked){
+            taskText.style.textDecoration = 'none';
+        }
+    });
+
+    deleteButton.addEventListener('click', function(){
+        todoList.removeChild(liItem);
+    });
 }
+//maybe make checkbox and delete seperate functions
